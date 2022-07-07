98e948e7be3573594656b05f4cad966d43e9fce7
Stern <90482851+SternI@users.noreply.github.com>
2022-07-08 01:36:08 +0200

    Add project files

---
+++ IniParser/ParseUtils.cs
@@ -0,0 +1,109 @@
+﻿using System.Runtime.InteropServices;
+using System.Text;
+
+namespace IniParser
+{
+    public class ParseUtils
+    {
+        string filePath;
+        string main = "Main";
+
+        [DllImport("kernel32", CharSet = CharSet.Unicode)]
+        static extern long WritePrivateProfileString(string Section, string Key, string Value, string FilePath);
+
+        [DllImport("kernel32", CharSet = CharSet.Unicode)]
+        static extern int GetPrivateProfileString(string Section, string Key, string Default, StringBuilder RetVal, int Size, string FilePath);
+
+        public ParseUtils(string iniFile = "")
+        {
+            filePath = new FileInfo(iniFile == "" ? main + ".ini" : iniFile).FullName;
+        }
+
+        public Dictionary<string, string> ParseIniDataWithSections()
+        {
+            var iniData = File.ReadAllLines(filePath);
+            var dictionary = new Dictionary<string, string>();
+            var rows = iniData.Where(t => !String.IsNullOrEmpty(t.Trim()) && !t.StartsWith(";") && (t.Contains('[') || t.Contains('=')));
+            if (rows == null || rows.Count() == 0) 
+                return dictionary;
+            string section = "";
+            foreach (string row in rows)
+            {
+                string rw = row.TrimStart();
+                if (rw.StartsWith("["))
+                    section = rw.TrimStart('[').TrimEnd(']');
+                else
+                {
+                    int index = rw.IndexOf('=');
+                    dictionary[section + "-" + rw.Substring(0, index).Trim()] = rw.Substring(index + 1).Trim().Trim('"');
+                }
+            }
+            return dictionary;
+        }
+
+        public string Read(string key, string section = "")
+        {
+            var retVal = new StringBuilder(255);
+            GetPrivateProfileString(section == "" ? main : section, key, "", retVal, 255, filePath);
+            return retVal.ToString();
+        }
+
+        public List<string> Read(List<string> keys, string section = "")
+        {
+            var retVal = new StringBuilder(255);
+            foreach (var key in keys)
+            {
+                GetPrivateProfileString(section == "" ? main: section, key, "", retVal, 255, filePath);
+            }
+            return new List<string> { retVal.ToString() };
+        }
+
+        public void Write(string key, string value, string section = "")
+        {
+            WritePrivateProfileString(section == "" ? main : section, key, value, filePath);
+        }
+
+        public void Write(List<string> keys, string value, string section = "")
+        {
+            foreach (var key in keys)
+            {
+                WritePrivateProfileString(section == "" ? main : section, key, value, filePath);
+            }
+        }
+
+        public void DeleteKey(string key)
+        {
+            var fileContents = File.ReadAllText(filePath);
+            fileContents = fileContents.Split("\n" + key)[0];
+            File.WriteAllText(filePath, fileContents);
+        }
+
+        public void DeleteKeys(List<string> keys)
+        {
+            foreach (var key in keys)
+            {
+                var fileContents = File.ReadAllText(filePath);
+                fileContents = fileContents.Split("\n" + key)[0];
+                File.WriteAllText(filePath, fileContents);
+            }
+        }
+
+        public void DeleteSection(string section = "")
+        {
+            Write("", "", section == "" ? main : section);
+        }
+
+        public void DeleteSections(List<string> sections = null)
+        {
+            foreach (var section in sections)
+            {
+                Write("", "", section == "" ? main : section);
+            }
+        }
+
+        public bool KeyExists(string key, string section = "")
+        {
+            return Read(key, section).Length > 0;
+        }
+    }
+}
---
+++ IniParser/Program.cs
@@ -0,0 +1,70 @@
+﻿using IniParser;
+using System.Reflection;
+
+Console.Title = "Stern's Ini Parser";
+
+Console.WriteLine("------------------------------------------------------------");
+Console.WriteLine("Initializing...");
+Console.WriteLine("------------------------------");
+
+Console.WriteLine("Please put your ini file path here:");
+
+var iniFile = Console.ReadLine();
+var parser = new ParseUtils(iniFile);
+
+Console.WriteLine("Select parse action.  Read Key = 0, Write Key = 1, Delete Key = 2, Delete Section = 3, Key Exists = 4");
+var action = Console.ReadLine();
+
+var key = "";
+if (action != "3")
+{
+    Console.WriteLine("Please put value key here:");
+    key = Console.ReadLine();
+}
+
+var section = "";
+Console.WriteLine("Want select a section or leave it default ('Main').  select = 0, leave it = 1");
+var answer = Console.ReadLine();
+
+if (answer == "0")
+{
+    Console.WriteLine("Please put a section here:");
+    section = Console.ReadLine();
+}
+
+switch (action)
+{
+    case "0":
+        string readedValue = parser.Read(key, section);
+        Console.WriteLine("Readed Value = '{0}'", readedValue);
+        break;
+    case "1":
+        Console.WriteLine("Please put a value here:");
+        var value = Console.ReadLine();
+        parser.Write(key, value, section);
+        Console.WriteLine("Successfully Wrote. {0}: '{1}'", key, value);
+        break;
+    case "2":
+        parser.DeleteKey(key);
+        Console.WriteLine("Successfully Deleted. '{0}'", key);
+        break;
+    case "3":
+        parser.DeleteSection(section);
+        Console.WriteLine("Successfully Deleted. '{0}'", section);
+        break;
+    case "4":
+        var exists = parser.KeyExists(key, section);
+        if (exists)
+            Console.WriteLine("{0} exists", key);
+        else
+            Console.WriteLine("{0} doesn't exists", key);
+        break;
+}
+
+Console.WriteLine("Want try another action?  yes = 0, no = 1");
+var refresh = Console.ReadLine() == "0";
+if (refresh)
+{
+    System.Diagnostics.Process.Start(Assembly.GetExecutingAssembly().GetName().Name);
+    Environment.Exit(0);
+}
